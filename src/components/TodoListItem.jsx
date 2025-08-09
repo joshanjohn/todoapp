d471e5bebc7876d658a0e4c1cece6c995eaf8f25
Joshan john <joshanjohn2003@gmail.com>
2025-08-09 12:40:01 +0100

    (chore) refactor css styling

--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,24 +1,17 @@
 import styles from "./todoListItem.module.css";
-export default function TodoListItem({ todo, setTasks, tasks}) {
-
-  function handleDelete(todo) {
+export default function TodoListItem({ todo, setTasks, tasks }) {
+  function onDelete(todo) {
     console.log("Delete todo :" + todo);
-    setTasks(tasks.filter((_todo) => _todo.name !== todo.name))
+    setTasks(tasks.filter((_todo) => _todo.name !== todo.name));
   }
 
   return (
     <div className={styles.todoListItem}>
-      <h4>
-        {todo.name}
-        <span>
-          <button
-            onClick={() => handleDelete(todo)}
-            className={styles.deleteBtn}
-          >
-            remove
-          </button>
-        </span>
-      </h4>
+      <h4>{todo.name}</h4>
+
+      <button className={styles.actionButton} onClick={() => onDelete(todo)}>
+        remove
+      </button>
     </div>
   );
 }
